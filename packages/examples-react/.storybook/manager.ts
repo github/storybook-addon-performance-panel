af412496185ad9d7d4a596e50e0c7960f244b6cb
Matthew Costabile <mattcosta7@github.com>
2026-02-22 21:58:45 +0000

    Add Universal Storybook cross-link to React storybook sidebar

--- packages/examples-react/.storybook/manager.ts
+++ packages/examples-react/.storybook/manager.ts
@@ -4,6 +4,11 @@ import {addons} from 'storybook/manager-api'
 setupManagerTheme(addons, {
   sidebarLinks: [
     {
+      label: '→ Universal Storybook',
+      localHref: 'http://examples-html.localhost:1355',
+      href: '/examples/universal/',
+    },
+    {
       label: '→ Documentation',
       localHref: 'http://site.localhost:1355',
       href: '/',
